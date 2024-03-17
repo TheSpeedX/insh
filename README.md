d14c6c0b34ddc64c1bb8ec4efcbd2344b8c7c41a
TheSpeedX <ggspeedx29@gmail.com>
2024-03-17 23:29:02 +0530

    Update the Readme and Setup

--- README.md
+++ README.md
@@ -1,37 +1,61 @@
-insh  (insulter shell)
-=========
 
-Insults You Whenever You Get An Error !!!
+# 🤖 InSH 
+![Version](https://img.shields.io/badge/version-2.0-blue?style=for-the-badge) ![Monthly Downloads](https://img.shields.io/pypi/dm/insh?style=for-the-badge) ![Views](https://img.shields.io/endpoint?style=for-the-badge&url=https%3A%2F%2Fhits.dwyl.com%2FTheSpeedX%2Finsh.json%3Fcolor%3Dblue)
 
-Installation
-=========
+Ever wished your shell had a personality? Meet [InSH](https://github.com/TheSpeedX/insh) - the Insulter Shell!
 
-`pip install insh`
 
-or install from source
+## Features
 
-`pip install git+https://github.com/TheSpeedX/insh.git`
+- Integrates with bash on windows and linux apparently
+- Helps you with command even if you haven't asked for it
+- Drains your OpenAI GPT credit for silly stuff
+
+## Installation
+
+Use the package manager pip:
+
+```bash
+pip install -U insh
+```
+
+Or alternatively, you can install the latest version using git:
+
+```bash
+pip install git+https://github.com/TheSpeedX/insh.git
+```
 
 ## Usage
 
+Configure the shell and OpenAI API:
+
+```bash
+insh --configure
 ```
-Usage: insh <commands>
 
+Activate/Deactivate the shell extension: 
+
+```bash
+insh --activate
+insh --deactivate
 ```
-Snapshot
-===========
-<img src="https://raw.githubusercontent.com/TheSpeedX/insh/master/test.png" alt="Running insh"><br>
-It Works !!!
 
-TODO
-=========
+Ask it any thing:
+```bash
+ina "<question>"
+insh --ask "<question>"
+```
 
--> Add A Shell
+NOTE: You might face issues with shell integration. Feel free to create an issue for it.
+
+## Screenshots
+
+![Working Stuff](https://github.com/TheSpeedX/insh/assets/42498830/6cbf71cb-69a5-4dad-987c-a2d585123985)
+
+![App Screenshot](https://github.com/TheSpeedX/insh/assets/42498830/11d7ee45-d365-4106-b1b3-7c95ea1c0455)
 
 
-Note
-----
+## Authors
 
-Its Heavily Under Work.... Report Any Bugs
+Made with 💔 by [@TheSpeedX](https://www.github.com/TheSpeedX)
 
-Made With ♥ By SpeedX
--- setup.py
+++ setup.py
@@ -1,37 +1,36 @@
 from setuptools import setup
 
 with open("README.md", "r") as fh:
-	long_description = fh.read()
+    long_description = fh.read()
+
 setup(
-	name = 'insh',
-	packages = ['insh'],
-    version = '1.1',
-    description = 'Insults You When You Get An Error',
+    name='insh',
+    packages=['insh'],
+    version='2.0',
+    description='A Python package that insults you when you encounter an error',
     long_description=long_description,
-	long_description_content_type="text/markdown",
+    long_description_content_type="text/markdown",
     author='TheSpeedX',
-    author_email='ggspeedx29@gmail.com',
-	url = 'https://github.com/TheSpeedX/insh',
-	download_url ="https://github.com/TheSpeedX/insh/archive/master.zip",
-	keywords = ['shell','funny','insult','python'],
-	data_files=[('', ['LICENSE'])],
-	classifiers=[
-	'Development Status :: 4 - Beta',
-	'Intended Audience :: Developers',
-	'Topic :: Software Development :: Libraries :: Python Modules',
-	'License :: OSI Approved :: MIT License',
-	'Programming Language :: Python :: 3',
-	'Programming Language :: Python :: 3.4',
-	'Programming Language :: Python :: 3.5',
-	'Programming Language :: Python :: 3.6',
-	'Programming Language :: Python :: 3.7',
-	'Operating System :: OS Independent',
-	'Environment :: Console',
-	],
-    license = 'MIT',
-	entry_points={
+    url='https://github.com/TheSpeedX/insh',
+    download_url="https://github.com/TheSpeedX/insh/archive/master.zip",
+    keywords=['shell', 'funny', 'gpt', 'python'],
+    data_files=[('', ['LICENSE'])],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Environment :: Console',
+    ],
+    license='MIT',
+    entry_points={
         'console_scripts': [
             'insh = insh.command_line:main',
         ],
-		}
+    },
+    install_requires=[
+        'openai>=1.13.3,<2',
+    ]
 )
